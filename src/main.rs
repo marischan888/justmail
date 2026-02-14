93641f5d3395d39f7c29683331b1b9de712d60db
marischan888 <marischancql@gmail.com>
2026-02-14 11:18:40 -0500

    Formatting code

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,7 @@ async fn health_check() -> impl Responder {
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().route("/health_check", web::get().to(health_check))
-    })
+    HttpServer::new(||  App::new().route("/health_check", web::get().to(health_check)))
     .bind("127.0.0.1:8000")?
     .run()
     .await
