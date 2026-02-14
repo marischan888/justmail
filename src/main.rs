3e0ad70a1a2ed392995983d07668dcdcc3c993e3
marischan888 <marischancql@gmail.com>
2026-02-14 15:03:39 -0500

    Add integration tests for health check, decoupling the main into lib

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-// handler
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
+use std::net::TcpListener;
+use justmail::run;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(||  App::new().route("/health_check", web::get().to(health_check)))
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    let listener = TcpListener::bind("127.0.0.1:8080")?;
+    run(listener)?.await
 }
