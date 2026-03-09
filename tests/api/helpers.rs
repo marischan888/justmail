e216f90cb68a26d26ee905efe0f1a8eca844b18e
marischan888 <marischancql@gmail.com>
2026-03-09 13:43:00 -0400

    Add new field base url in ApplicationSettings for local development testing

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,3 +1,4 @@
+use linkify::{LinkFinder, LinkKind};
 use justmail::configuration::{get_configuration, DatabaseSettings};
 use justmail::startup::{get_connection_pool, Application};
 use justmail::telemetry::{get_subscriber, init_subscriber};
@@ -5,6 +6,7 @@ use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use once_cell::sync::Lazy;
 use reqwest::{Response};
+use wiremock::MockServer;
 
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
@@ -22,6 +24,14 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 pub struct TestApp {
     pub db_pool: PgPool,
     pub address: String,
+    pub email_server: MockServer,
+    // port for test in dev
+    pub port: u16,
+}
+
+pub struct ConfirmationLinks {
+    pub html_link: reqwest::Url,
+    pub plain_text: reqwest::Url,
 }
 
 impl TestApp {
@@ -34,15 +44,42 @@ impl TestApp {
             .await
             .expect("Failed to execute request.")
     }
+
+    pub fn get_confirmation_links(&self, request: &wiremock::Request) -> ConfirmationLinks {
+        let body: serde_json::Value = serde_json::from_slice(&request.body).unwrap();
+
+        // Extract the link from one of the request field
+        let get_link = |s: &str| {
+            let links: Vec<_> = LinkFinder::new()
+                .links(s)
+                .filter(|x| {*x.kind() == LinkKind::Url})
+                .collect();
+            assert_eq!(links.len(), 1);
+            let raw_link = links[0].as_str().to_owned();
+            let mut confirmation_link = reqwest::Url::parse(&raw_link).unwrap();
+            // Let's make sure we don't call random APIs on the web
+            assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
+            confirmation_link.set_port(Some(self.port)).unwrap();
+            confirmation_link
+        };
+
+        let html_link = get_link(&body["HtmlBody"].as_str().unwrap());
+        let plain_text = get_link(&body["TextBody"].as_str().unwrap());
+
+        ConfirmationLinks { html_link, plain_text }
+    }
 }
 
 pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
 
+    let email_server = MockServer::start().await;
+
     let configurations = {
         let mut config = get_configuration().expect("Failed to read configuration.");
         config.database.database_name = Uuid::new_v4().to_string();
         config.application.port = 0;
+        config.email_client.base_url = email_server.uri();
         config
     };
 
@@ -51,13 +88,15 @@ pub async fn spawn_app() -> TestApp {
     let application = Application::build(configurations.clone())
         .await
         .expect("Failed to build application.");
-
+    let application_port = application.port();
     let address = format!("http://127.0.0.1:{}", application.port());
     let _ = tokio::spawn(application.run_until_stopped());
 
     TestApp {
         db_pool: get_connection_pool(&configurations.database),
         address,
+        email_server,
+        port: application_port,
     }
 }
 
