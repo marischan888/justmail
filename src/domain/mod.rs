8fe227182c1cfac7cb4fb9f5a64ec72c4a405743
marischan888 <marischancql@gmail.com>
2026-07-22 11:07:23 -0400

    Replace domain mod with domain.rs

--- src/domain/mod.rs
+++
@@ -1,7 +0,0 @@
-mod subscriber_email;
-mod subscriber_name;
-mod new_subscriber;
-
-pub use subscriber_name::SubscriberName;
-pub use new_subscriber::NewSubscriber;
-pub use subscriber_email::SubscriberEmail;
